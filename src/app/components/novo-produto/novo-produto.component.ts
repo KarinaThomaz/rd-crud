15ee1467ba7b88de3edd497cee042e9e07777b97
Treinamento <Treinamento@RD723924.raiadrogasil.com>
2020-03-06 18:00:49 -0300

    Projeto Sergio

---
+++ src/app/components/novo-produto/novo-produto.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
+import { Produto } from 'src/app/models/Produto';
+
+
+@Component({
+  selector: 'app-novo-produto',
+  templateUrl: './novo-produto.component.html',
+  styleUrls: ['./novo-produto.component.css']
+})
+export class NovoProdutoComponent implements OnInit {
+
+  formProduto:FormGroup;
+
+  constructor() { }
+
+  createForm(produto: Produto){
+    this.formProduto=new FormGroup({
+      cod: new FormControl(produto.codigo),
+      desc: new FormControl(produto.desc),
+      reg_anvisa: new FormControl(produto.reg_anvisa),
+      controlado: new FormControl(produto.controlado),
+      cod_grupo: new FormControl(produto.cod_grupo)
+    })
+  }
+
+  ngOnInit(): void {
+    this.createForm(new Produto());
+  }
+
+  enviarProduto(){
+    console.log(this.formProduto);
+  }
+
+}
